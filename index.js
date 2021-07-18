299a267b01af31d7daa6d6d8e1acb81a4da484df
Olivier Mantel <olivier.mantel@sage.com>
2021-07-18 17:12:55 +0200

    new version works

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
+// packages
 const fs = require('fs');
 const process = require('process');
 //const rmrf = require('rmrf');
 const { exec, execSync } = require('child_process');
 const path = require('path');
-const config=require('./config.json')
+const config=require('./config.json');
+//const { spawn } = require('child_process');
 
-let dir=""
-let dirExecInstall=""
-let dirSetInstall=""
+// Functions
 function changeDir(dir) {
   console.log("<-- Start :","Change directory",dir)
-  console.log("Current directory",dir)  
   process.chdir(dir);
   console.log("success");
-  console.log("Current directory",dir)
-  console.log("<-- End :","Change directory",dir)
+  console.log("<-- End   :","Change directory",dir)
+  console.log("")
 }
 
 function createDir(dir){
@@ -25,13 +24,14 @@ function createDir(dir){
     else
      console.log("success");
 });
-console.log("<-- End :","Creation directory",dir)
+console.log("<-- End   :","Creation directory",dir)
+console.log("")
 }
 
 function execCmd(cmd) {
-  console.log("<-- Start :","Execution command",cmd)
+  
   exec(cmd, (error, stdout, stderr) => {
-    
+    console.log("<-- Start :","Execution command",cmd)
     if (error) {
         console.error("error",error);
         return;
@@ -41,75 +41,118 @@ function execCmd(cmd) {
         return;
     }
     console.log(`stdout: ${stdout}`);
+    console.log("<-- End   :","Execution command",cmd)
+    console.log("")
 });
-console.log("<-- End :","Execution command",cmd)
+
 }
 
 function execCmdSync(cmd) {
   console.log("<-- Start :","Execution command",cmd)
-  execSync(cmd, (error, stdout, stderr) => {
-    
-    if (error) {
-        console.error("error",error);
-        return;
-    }
-    if (stderr) {
-      console.log("success",stderr);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
-console.log("<-- End :","Execution command",cmd)
+  execSync(cmd)
+  console.log("<-- End   :","Execution command",cmd)
+  console.log("")
 }
 
+function transformMsToSMMS(ms_s) {
+ //console.log("ms_s",ms_s)
+ const  s_dec = ms_s/1000
+ //console.log("s_dec",s_dec)
+ const  s = Math.trunc(ms_s/1000)
+ //console.log("s",s)
+ const  ms_d= Math.round((s_dec-s)*1000)
+ //console.log("ms_d",ms_d)
+ const mn = Math.trunc(s/60)
+ //console.log("mn",mn)
+ const s_r = s-mn*60
+ //console.log("s_r",s_r)
+ //console.log(mn,s_r,ms_d)
+ return mn+":"+s_r+":"+ms_d
+}
+
+// Main script
+function main() {
+const git=config.git
+
+let dir=""
+let dirExecInstall=""
+let dirSetInstall=""
+let cmds=""
 
 
 try {
-    
+  
   console.log("<-- Start :","Installation practice-x3")
   dirExecInstall=process.cwd()
   console.log("-- Directory : execution",dirExecInstall)
-  dirSetInstall=config.dir.dir_install
+  dirSetInstall=config.dir["dir-install"]
   console.log("-- Directory : installation",dirSetInstall)
   
   
-  //dir=path.join(dirExecInstall,"..")
-    //changeDir(dir) 
-    dir=dirSetInstall
     /*rmrf(dir, function(err) {
       done();
     });
     */
-    createDir(dir)
-    changeDir(dir)
-    //dir=path.join(dirSetInstall,"practice-x3-jwt")
-    //createDir(dir)
-    //changeDir(dir)
-    execCmdSync("git clone "+config.git.practice_x3_jwt)
-    //dir=path.join(dirSetInstall,"practice-x3-graphql-server")
-    //createDir(dir)
-    //changeDir(dir)
-   
-    execCmdSync("git clone "+config.git.practice_x3_graphql_server)
-    //createDirBeforeExecCmd(dir,"git clone https://github.com/oliviermantel/practice-x3-graphql-server.git")
-    dir=path.join(dirSetInstall,"practice-x3-jwt")
-    changeDir(dir)
-    execCmdSync("npm install")
-    execCmdSync("npm run test ")
-    dir=path.join(dirSetInstall,"practice-x3-graphql-server")
-    changeDir(dir)
-    execCmdSync("npm install")
-    //
-    //console.log("---> End installation practice-x3");
+    createDir(dirSetInstall)
+    
+
+    for (const key in git) {
+      if (git.hasOwnProperty.call(git, key)) {
+        //const element = git[key];
+        //console.log(key,element)
+        changeDir(dirSetInstall)
+        //cmds = "git clone "+config.git[key]
+        //cmds += " &&"
+        //cmds += " cd "+ key
+        //cmds += " &&"
+        //cmds += " npm install || true "
+  //      cmds += " &&"
+  //      cmds += " npm run test"
+  //performance.mark(key+' execCmd-start')
+         execCmdSync("git clone "+config.git[key])
+         changeDir(key)
+         execCmdSync("npm install")
+        //performance.mark(key+' execCmd-end')
+        //performance.measure(key+' execCmd', key+' execCmd-start', key+' execCmd-end')
+      }
+    }
     
   } catch (err) {
-    // Printing error if occurs
     console.error("error",err);
   } finally {
-    console.log("<-- End :","Installation practice-x3")
+    console.log("<-- End   :","Installation practice-x3")
+    
+	//performance.measure('install', 'execCmd-start', 'execCmd-end')
+    
   }
   
-
+}
  
+//const startTime = new Date().getTime();
+//performance.mark("start-script")
+//const { performance, PerformanceObserver } = require("perf_hooks");
+//const { NOTIMP } = require('dns');
+//const { debug } = require('console');
+//const axios = require('axios')
+//const customLogger = require('our-custom-logging-solution')
 
+//const perfObserver = new PerformanceObserver((items) => {
+//  console.log(items.getEntries()[0].duration);
+//  performance.clearMarks();
+//})
+
+//perfObserver.observe({ entryTypes: ["measure"], buffer: true })
+//let dateStart = new Date()
+//let hrStart = process.hrtime()
+//var simulateTime = 5
+console.time("Duration script")
+main()
+console.timeEnd("Duration script")
+//let dateEnd = new Date() - dateStart
+//hrEnd = process.hrtime(hrStart)
+
+//console.log('Execution time: %dms', dateEnd)
+//console.log('Execution time (hr): %ds %dms', hrEnd[0], hrEnd[1] / 1000000)
+//const elapsedTime = new Date().getTime() - startTime;
+//console.log("ElapsedTime (mn:s:ms)="+transformMsToSMMS(elapsedTime));
 
